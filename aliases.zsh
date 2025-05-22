be3bd7a2bb917fde30254a872dfbdba0f0af2c29
clopezpro <khrystyan881@hotmail.com>
2025-05-22 16:34:44 -0500

    my config

---
+++ aliases.zsh
@@ -0,0 +1,18 @@
+alias wind="windsurf"
+alias nd="npm run dev"
+alias ndh="npm run dev -- --host"
+alias gs="git status"
+alias ls="ls -al"
+alias master="git checkout master"
+alias gch="git checkout"
+
+alias gb='git branch'
+alias gbd='git branch -d'
+
+
+#para ir mis proyectos , solo ejecutar  i/carpeta_proyecto
+
+
+function i() {
+  cd ~/Documentos/developer/$1
+}
